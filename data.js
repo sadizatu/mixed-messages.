f3065bceb0f9a3e03dd690fa913c746b9b4c2ad1
KARFO <adizatu.karfo@amalitech.org>
2022-03-21 21:55:50 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,28 @@
+const love = [
+    "I saw that you were perfect, and so I loved you. Then I saw that you were not perfect and I loved you even more",
+    "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams",
+    "Love is that condition in which the happiness of another person is essential to your own",
+    "The best thing to hold onto in life is each other",
+    "I need you like a heart needs a beat",
+    "I am who I am because of you. You are every reason, every hope, and every dream I’ve ever had",
+    "If I had a flower for every time I thought of you… I could walk through my garden forever",
+    "Take my hand, take my whole life too. For I can’t help falling in love with you",
+    "If you live to be a hundred, I want to live to be a hundred minus one day so I never have to live without you",
+    "You’re the closest to heaven, that I’ll ever be",
+    "You are the finest, loveliest, tenderest, and most beautiful person I have ever known and even that is an understatement",
+    "I will never stop trying. Because when you find the one… you never give up",
+    "It’s always better when we’re together",
+    "I love you for all that you are, all that you have been and all that you will be",
+    "To the world you may be one person, but to one person you are the world",
+    "I’ve tried so many times to think of a new way to say it, and it’s still I love you",
+    "I love you and that’s the beginning and end of everything",
+    "If I know what love is, it is because of you",
+    "If you remember me, then I don’t care if everyone else forgets",
+    "We loved with a love that was more than love",
+]
+
+const grief = [
+
+]
+
+console.log(love.length);
